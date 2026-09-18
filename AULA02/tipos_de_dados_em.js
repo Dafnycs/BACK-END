c3f4ca417badc6e24fe15452d2df13e90059261d
Dafny <dcalvanesesiqueira@gmail.com>
2026-09-18 14:36:05 -0300

    BACK-END

---
+++ AULA02/tipos_de_dados_em.js
@@ -0,0 +1,147 @@
+//1- String (Textos)
+let String = "Parte de String:"
+console.log(String)
+//Exercício 1 - Crie uma variável nome com seu nome e exiba no console.
+let nome = "Dafny";
+console.log (nome);
+
+//Exercício 2 - Crie uma variável sobrenome e mostre nome + sobrenome.
+let Nome = "Dafny";
+let Sobrenome = " Calvanese";
+let conjunto = Nome + Sobrenome;
+console.log("Meu nome é: " +  conjunto);
+
+//Exercício 3 - Crie uma frase usando Template String.
+let celular = "Iphone";
+console.log (`Qual é seu celular: ${celular} 14. ` )
+
+//Exercício 4 - Guarde o nome de uma escola e exiba.
+let escola = "Sesi";
+console.log(escola);
+
+//Exercício 5 - Crie duas strings e junte em uma frase.
+let Estado = "Rio de Janeiro";
+let Cidade = ", Duque de Caxias";
+let Conjunto = Estado + Cidade;
+console.log ("Fala um estado e uma cidade:" + Conjunto);
+
+//Exercício 6 - Exiba uma mensagem de boas-vindas.
+let mensagem = 'Boas-vindas.';
+console.log (mensagem);
+
+//Exercício 7 - Mostre o tamanho de uma string.
+let qualTimeVocetorce = "Palemiras!";
+console.log(qualTimeVocetorce.length)
+
+//Exercício 8 - Transforme um texto em letras maiúsculas.
+let regra = "não pode usar o celular na escola";
+console.log(regra.toUpperCase())
+
+
+//Exercício 9 - Transforme um texto em letras minúsculas.
+let Regra = "TEM QUE USAR UNIFORME"
+console.log(Regra.toLowerCase())
+
+//Exercício 10 - Crie uma frase com nome e cidade.
+let cidade = "São Paulo"
+console.log (`Em qual cidade voce mora: ${cidade}.`)
+
+
+//2- Number (Números)
+let Number = "Parte de Number:"
+console.log(Number)
+//Exercício 1 - Crie dois números e exiba a soma.
+let soma = 1430 + 5432;
+console.log(soma)
+
+//Exercício 2 - Calcule a subtração.
+let subtracao = 10430 - 5432;
+console.log(subtracao)
+
+//Exercício 3 - Calcule a multiplicação.
+let multiplicacao = 100 * 5432;
+console.log(multiplicacao)
+
+//Exercício 4 - Calcule a divisão.
+let divisao = 50 / 5;
+console.log(divisao)
+
+//Exercício 5 - Calcule a média de dois números.
+let numero1 = 50;
+let numero2 = 2;
+let media = (numero1 + numero2) / 2;
+console.log("A média de 50 e 2 é: " + media);
+
+
+//Exercício 6 - Calcule o dobro de um número.
+let numerodobro = 100;
+let dobro = numerodobro * 5;
+console.log(dobro);
+
+//Exercício 7 - Calcule o resto da divisão.
+let divisor = 10;
+let por = 3;
+let resto = divisor % por;
+console.log(resto);
+
+//Exercício 8 - Crie um preço e um desconto.
+let preco = 200;
+let desconto = 20;
+let valordesconto = preco * (desconto / 100);
+let precofinal = preco - valordesconto;
+console.log("Uma blusa custa 200 reais, mas esta com 20% de desconto: " + precofinal);
+
+
+//Exercício 9 - Converta idade em meses.
+let idade = 17
+let meses = 12
+let coonjuto = idade * meses 
+console.log (coonjuto)
+
+//Exercício 10 - Calcule o quadrado de um número.3
+let quadrado = 3 ** 2
+console.log (quadrado)
+
+
+
+
+
+//3- Boolean (Verdadeiro ou Falso)
+let Boolean = "Parte de Boolean:"
+console.log(Boolean)
+//Exercício 1 - Crie uma variável ativo com true.
+let idadee = 19
+let podeDirigir = idadee >= 18;
+console.log(podeDirigir);
+
+//Exercício 2 - Verifique se um número é maior que 10.
+let numero = 15
+let maior = numero <= 10;
+console.log(maior);
+
+
+//Exercício 3 - Verifique se a idade é maior ou igual a 18.
+let idaade = 17
+let Maioridade = idaade >= 18;
+console.log(Maioridade);
+
+
+//Exercício 4 - Compare dois números iguais.
+let primeironum = 5;
+let segundonum = 5;
+console.log(primeironum === segundonum);
+
+
+//Exercício 5 - Compare dois números diferentes.
+let prinumm = 3;
+let segnumm = 5;
+console.log(prinumm === segnumm);
+
+//Exercício 6 - Verifique se um número é par.
+
+
+
+//Exercício 7 - Use AND (&&) em uma condição.
+//Exercício 8 - Use OR (||) em uma condição.
+//Exercício 9 - Negue um valor boolean.
+//Exercício 10 - Compare texto e número.
