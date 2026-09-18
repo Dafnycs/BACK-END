39b507cf0c1c47fb0b79672eea778ef3e4edd673
Dafny <dcalvanesesiqueira@gmail.com>
2026-09-18 09:44:01 -0300

    BACK-END

---
+++ AULA02/conceito.js
@@ -0,0 +1,42 @@
+var cor = "Azul";
+var cor = "Vermelho";
+console.log(cor);
+
+let fruta = "Manga";
+fruta = "Uva";
+console.log(fruta);
+
+const pi = 3.14;
+// pi = 3.15; vai dar erro, porque cost é uma fixa
+console.log(pi)
+
+//---------------------------------
+// FORMAS DE ESCREVER UM CÓDIGO
+//---------------------------------
+
+
+// CAMEL CASE --- * A mais Famosa
+// - Primeira palavra minúscula
+// - palavras seguintes começam com maiúscula
+
+
+//let nomeCompleto; let idadeUsuario; functionCalcularIdade(){}
+
+//________________________________________________
+//  PASCALCASE
+// -Todas as palavras começam com letra maiúscula
+
+//  class UsuarioSistema {
+//   constructor(nome, idade) {
+//     this.nome = nome;
+//       this.nome = idade;
+//    }
+// }:
+
+
+
+//________________________________________________
+//  SNAKE CASE
+// - Palavras separadas por underscore_
+
+//let nomw_completo; let total_vendas;
