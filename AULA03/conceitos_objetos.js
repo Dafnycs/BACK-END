4bc8ced20bd61a3cbc5d394e0442b5950a2f39aa
dafny <dcalvanesesiqueira@gmail.com>
2026-09-25 08:33:30 -0300

    aula-back

---
+++ AULA03/conceitos_objetos.js
@@ -0,0 +1,10 @@
+// object (Objetos)
+// Um objeto agrupa várias informações dentro de mesma variável, usando chave e valor
+
+let pessoa = {
+    nome: "João",
+    idade: 16,
+    cidade: "São paulo"
+};
+console.log(pessoa.nome);
+console.log(pessoa.idade);
